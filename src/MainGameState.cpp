5fcf02ed437f6e23fb9a1e5a6118cc75d9b16c3d
Javier Sánchez Villaescusa <jsv39@mscloud.ua.es>
2025-09-23 15:13:56 +0200

    Añadida bola roja con fisicas que salta con el espacio

--- src/MainGameState.cpp
+++ src/MainGameState.cpp
@@ -4,29 +4,38 @@ extern "C" {
  #include <raylib.h>
 }
 
+
+
 MainGameState::MainGameState()
 {
-    BeginDrawing();
-    DrawText("Bienvenido a Flappy Bird DCA", 50, 50, 5, YELLOW);
-    EndDrawing();
+    
 }
 
 void MainGameState::init()
 {
-
+    player.x = 200;
+    player.y = 200;
 }
 
 void MainGameState::handleInput()
 {
-
+    if(IsKeyPressed(KEY_SPACE)){
+        player.vy += -300;
+    }
 }
 
 void MainGameState::update(float deltaTime)
 {
-
+    player.vy += 98 * deltaTime;
+    player.y += player.vy * deltaTime;
 }
 
 void MainGameState::render()
 {
 
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Bienvenido a Flappy Bird DCA", 50, 50, 15, YELLOW);
+    DrawCircle(player.x, player.y, 17, RED);
+    EndDrawing();
 }
---
+++ vendor/include/MainGameState.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <GameState.hpp>
+
+
+struct Bird{
+    float x;
+    float y;
+    float vy;
+};
+
+class MainGameState : public GameState
+{
+    public:
+        MainGameState();
+        ~MainGameState() = default;
+
+        void init() override;
+        void handleInput() override;
+        void update(float deltaTime) override;
+        void render() override;
+
+        void pause(){};
+        void resume(){};
+
+    
+    private:
+        char entered_key;
+        Bird player;
+};
